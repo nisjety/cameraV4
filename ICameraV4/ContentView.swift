a444b0f667d85492a9ff12dc6d5adf7267c3be37
nisjety <ima_stovers24@hotmail.com>
2024-08-15 21:38:20 +0200

    Initial Commit

---
+++ ICameraV4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ICameraV4
+//
+//  Created by Ima Da Costa on 15/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ICameraV4/ICameraV4App.swift
@@ -0,0 +1,17 @@
+//
+//  ICameraV4App.swift
+//  ICameraV4
+//
+//  Created by Ima Da Costa on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ICameraV4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
